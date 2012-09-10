7477e8021581b84147b70ad0fe54706c27ac1d29
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2012-09-10 14:40:17 +0200

    Changed licence to GPLv3

--- scripts/thirdparties/import-thirdparties-csv.php
+++ scripts/thirdparties/import-thirdparties-csv.php
@@ -1,12 +1,14 @@
 #!/usr/bin/php
 <?php
-/* Copyright (C) 2007-2011 Laurent Destailleur  <eldy@users.sourceforge.net>
+/* Copyright (C) 2007-2011 Laurent Destailleur <eldy@users.sourceforge.net>
  * Copyright (C) 2012 Cédric Salvador <csalvador@gpcsolutions.fr>
  * Copyright (C) 2012 Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
  *
- * This program is free software; you can redistribute it and/or modify
+ * Third parties import script for Dolibarr
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -15,7 +17,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**

1fa6def21449d301464e2dc914cd49f6fcc3c9b2
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2012-12-18 18:32:36 +0100

    First working implementation

--- scripts/products/import-purchase-prices-csv.php
+++ scripts/products/import-purchase-prices-csv.php
@@ -38,7 +38,6 @@ require_once($path . "../../htdocs/master.inc.php");
 // After this $db, $mysoc, $langs and $conf->entity are defined. Opened database handler will be closed at end of file.
 
 require_once(DOL_DOCUMENT_ROOT . "/product/class/product.class.php");
-require_once(DOL_DOCUMENT_ROOT . "/categories/class/categorie.class.php");
 require_once(DOL_DOCUMENT_ROOT."/fourn/class/fournisseur.product.class.php");
 
 @set_time_limit(0);  // No timeout for this script
@@ -90,15 +89,104 @@ if (($handle = fopen($fname, 'r')) !== FALSE) {
 	 		continue; // Ignores first line
 			// TODO: Test that first line is what we expect
 		}
-		// TODO: search product
-		// $product_ref = trim($data[0]);
-		// TODO: search supplier
-		// $supplier_list = explode(',', trim($data[1]));
-		foreach ($supplier_list as $supplier_name) {
-			// TODO: test if there's already a purchase price
-			// TODO: if not, set the listed price
-			// $purchase_price = trim($data[2]);
+		// Search product
+		$product_ref = trim($data[0]);
+		$product = new ProductFournisseur($db);
+		$sql = 'SELECT rowid';
+		$sql .= ' ';
+		$sql .= 'FROM ' . MAIN_DB_PREFIX . 'product';
+		$sql .= ' ' ;
+		$sql .= 'WHERE ref="' . $product_ref . '"';
+		$sql .= 'AND entity IN (' . $conf->entity . ')';
+		$resql = $db->query($sql);
+		unset($sql);
+		if ($resql && ($resql->num_rows != 0)) {
+			// FIXME: Check unicity !!!
+			$res = $db->fetch_array($resql);
+			$product_id = $res['rowid'];
+			$product->fetch($product_id);
+			$db->free($resql);
+			unset($res);
+		} else {
+			$error ++;
+			printLine($line);
+			print "Should never be there\n";
 		}
+		unset($resql);
+
+		// Search supplier
+		$supplier_name = trim($data[1]);
+		$supplier = new Fournisseur($db);
+		$sql = 'SELECT rowid';
+		$sql .= ' ';
+		$sql .= 'FROM ' . MAIN_DB_PREFIX . 'societe';
+		$sql .= ' ' ;
+		$sql .= 'WHERE nom="' . $supplier_name . '"';
+		$sql .= ' ';
+		$sql .= 'AND fournisseur=1';
+		$sql .= ' ';
+		$sql .= 'AND entity IN (' . $conf->entity . ')';
+		$resql = $db->query($sql);
+		unset($sql);
+		if ($resql && ($resql->num_rows != 0)) {
+			// FIXME: Check unicity !!!
+			$res = $db->fetch_array($resql);
+			$supplier_id = $res['rowid'];
+			$supplier->fetch($supplier_id);
+			$db->free($resql);
+			unset($res);
+		} else {
+			$error ++;
+			printLine($line);
+			print "Should never be there\n";
+		}
+		unset($resql);
+		// Test if there's already a purchase price
+		$sql = 'SELECT rowid, price';
+		$sql .= ' ';
+		$sql .= 'FROM ' . MAIN_DB_PREFIX . 'product_fournisseur_price';
+		$sql .= ' ' ;
+		$sql .= 'WHERE fk_product="' . $product_id . '"';
+		$sql .= ' ';
+		$sql .= 'AND fk_soc="' . $supplier_id . '"';
+		$sql .= ' ';
+		$sql .= 'AND entity IN (' . $conf->entity . ')';
+		$resql = $db->query($sql);
+		unset($sql);
+		if ($resql && ($resql->num_rows != 0)) {
+			// FIXME: Check unicity !!!
+			$res = $db->fetch_array($resql);
+			$supplier_product_price = $res['price'];
+			$supplier_product_id = $res['rowid'];
+			if ($supplier_product_price != '0') {
+				// TODO: make skip optionnal
+				printLine($line);
+				print "skipped because there's already a price set\n";
+				$line--;
+				continue; // We skip this line
+			}
+			$product->product_fourn_price_id = $supplier_product_id;
+			$db->free($resql);
+			unset($res);
+		}
+		// Set the listed price
+		$supplier_ref = trim($data[2]);
+		$quantity = trim($data[3]);
+		$purchase_price = trim($data[4]);
+		$price_base_type = 'HT';
+		$availability = '';
+		$vat_rate = trim($data[5]);
+		$ret = $product->update_buyprice(
+			$quantity,
+			$purchase_price,
+			$user,
+			$price_base_type,
+			$supplier,
+			$availability,
+			$supplier_ref,
+			$vat_rate
+		);
+		// TODO: handle $ret error
 	}
 	fclose($handle);
 } else {

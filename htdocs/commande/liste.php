41631ba152a79bcea481306d685b17b302b4b427
Antoine Iauch <aiauch@gpcsolutions.fr>
2013-01-24 18:04:43 +0100

    Add a "VAT-free" Amount on Customer Orders

--- htdocs/commande/liste.php
+++ htdocs/commande/liste.php
@@ -5,6 +5,7 @@
  * Copyright (C) 2005-2012 Regis Houssin          <regis.houssin@capnetworks.com>
  * Copyright (C) 2012      Juanjo Menent          <jmenent@2byte.es>
  * Copyright (C) 2013      Christophe Battarel    <christophe.battarel@altairis.fr>
+ * Copyright (C) 2013      Raphaël Doursenaud     <rdoursenaud@gpcsolutions.fr>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -43,6 +44,7 @@ $deliveryyear=GETPOST("deliveryyear","int");
 $deliverymonth=GETPOST("deliverymonth","int");
 $sref=GETPOST('sref','alpha');
 $sref_client=GETPOST('sref_client','alpha');
+$samount=GETPOST('samount','alpha');
 $snom=GETPOST('snom','alpha');
 $sall=GETPOST('sall');
 $socid=GETPOST('socid','int');
@@ -81,7 +83,7 @@ if (GETPOST("button_removefilter_x"))
     $search_ref='';
     $search_refcustomer='';
     $search_societe='';
-    $search_montant_ht='';
+    $samount='';
     $orderyear='';
     $ordermonth='';
     $deliverymonth='';
@@ -126,6 +128,9 @@ if ($sref)
 if ($sall)
 {
 	$sql.= " AND (c.ref LIKE '%".$db->escape($sall)."%' OR c.note LIKE '%".$db->escape($sall)."%')";
+}
+if ($samount) {
+    $sql.= " AND c.total_ht = '".$db->escape(trim($samount))."'";
 }
 if ($viewstatut <> '')
 {
@@ -290,7 +295,8 @@ if ($resql)
 	print '<input class="flat" type="text" size="10" name="sref_client" value="'.$sref_client.'">';
 	print '</td><td class="liste_titre">&nbsp;';
 	print '</td><td class="liste_titre">&nbsp;';
-	print '</td><td class="liste_titre">&nbsp;';
+	print '</td><td class="liste_titre" align="right">';
+	print '<input class="flat" type="text" name="samount" value="'.$samount.'">';
 	print '</td><td align="right" class="liste_titre">';
 	print '<input type="image" class="liste_titre" name="button_search" src="'.DOL_URL_ROOT.'/theme/'.$conf->theme.'/img/search.png"  value="'.dol_escape_htmltag($langs->trans("Search")).'" title="'.dol_escape_htmltag($langs->trans("Search")).'">';
 	print '</td></tr>';
@@ -372,6 +378,13 @@ if ($resql)
 		print $d;
 		print ' <a href="'.$_SERVER['PHP_SELF'].'?deliveryyear='.$y.'&amp;deliverymonth='.$m.'">'.$ml.'</a>';
 		print ' <a href="'.$_SERVER['PHP_SELF'].'?deliveryyear='.$y.'">'.$y.'</a>';
+		print '</td>';
+
+		// Amount
+		print '<td align="right">';
+		print price($objp->total_ht);
+		print '&nbsp;';
+		print getCurrencySymbol($conf->currency);
 		print '</td>';
 
 		// Amount HT

73513ef2f0cfd6f0427d9234a0ecc77d5ea725b6
Cédric Salvador <csalvador@gpcsolutions.fr>
2013-06-10 18:10:06 +0200

    Preselected supplier when there's only one

--- htdocs/core/class/html.form.class.php
+++ htdocs/core/class/html.form.class.php
@@ -1776,7 +1776,7 @@ class Form
      *  @param      string	$htmlname        Name of HTML field
      *  @return		void
      */
-    function select_product_fourn_price($productid,$htmlname='productfournpriceid')
+    function select_product_fourn_price($productid,$htmlname='productfournpriceid', $showempty=0)
     {
         global $langs,$conf;
 
@@ -1807,9 +1807,15 @@ class Form
             {
                 $form.= '<option value="0">-- '.$langs->trans("NoSupplierPriceDefinedForThisProduct").' --</option>';
             }
+            else if ($num == 1) {
+                $objp = $this->db->fetch_object($result);
+                $form = $objp->nom;
+                $form .= '<input type="hidden" name="'.$htmlname.'" value="'.$objp->idprodfournprice.'">';
+            }
             else
             {
-                $form.= '<option value="0">&nbsp;</option>';
+                $form = '<select class="flat" name="'.$htmlname.'">';
+                if($showempty) $form.= '<option value="0">&nbsp;</option>';
 
                 $i = 0;
                 while ($i < $num)

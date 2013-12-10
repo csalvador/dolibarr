3bb34140c27c5826216d0f1f2e12003e0709e757
Raphaël Doursenaud <rdoursenaud@gpcsolutions.fr>
2013-12-10 11:57:54 +0100

    Added missing language loads for bookmarks

--- htdocs/bookmarks/liste.php
+++ htdocs/bookmarks/liste.php
@@ -24,6 +24,7 @@
 require '../main.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/bookmarks/class/bookmark.class.php';
 
+$langs->load("bookmarks");
 
 $sortfield = GETPOST("sortfield",'alpha');
 $sortorder = GETPOST("sortorder",'alpha');
